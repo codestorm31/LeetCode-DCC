912995d1c6a8aa8f3fca4660f6856fece3aa905d
Aryan Saji <aryansaji3103@gmail.com>
2024-06-02 19:51:04 +0530

    344. LeetCode-DCC

---
+++ ReverseString.cpp
@@ -0,0 +1,15 @@
+/*
+    344. Reverse String
+    Question-Link: https://leetcode.com/problems/reverse-string/description/
+*/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        for(int i=0;i<n/2;i++){
+            swap(s[i],s[n-i-1]);
+        }
+    }
+};
+
